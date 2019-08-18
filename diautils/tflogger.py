7336441553d3e984d2a76934ebb6f14cf469aa3c
Eric Renault <eric.renault.info@gmail.com>
2019-08-18 11:45:53 +0200

    .

--- diautils/tflogger.py
+++ diautils/tflogger.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import numpy as np
 
 class TfLogger:
 
@@ -9,13 +9,48 @@ class TfLogger:
     def log_all(self, summaries, step):
         for summary in summaries:
             self.log(summary, step)
+        return self
 
     def log(self, summary, step):
         self.writer.add_summary(summary, step)
+        return self
+
+    def log_hist(self, tag, values, step, bins=1000):
+        """
+        Logs the histogram of a list/vector of values.
+        From: https://gist.github.com/gyglim/1f8dfb1b5c82627ae3efcfbbadb9f514
+        """
+
+        # Create histogram using numpy
+        counts, bin_edges = np.histogram(values, bins=bins)
+
+        # Fill fields of histogram proto
+        hist = tf.HistogramProto()
+        hist.min = float(np.min(values))
+        hist.max = float(np.max(values))
+        hist.num = int(np.prod(values.shape))
+        hist.sum = float(np.sum(values))
+        hist.sum_squares = float(np.sum(values ** 2))
+
+        # Requires equal number as bins, where the first goes from -DBL_MAX to bin_edges[1]
+        # See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/framework/summary.proto#L30
+        # Therefore we drop the start of the first bin
+        bin_edges = bin_edges[1:]
+
+        # Add bin edges and counts
+        for edge in bin_edges:
+            hist.bucket_limit.append(edge)
+        for c in counts:
+            hist.bucket.append(c)
+
+        # Create and write Summary
+        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, histo=hist)])
+        return self.log(summary, step)
 
     def log_scalar(self, tag, value, step):
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.log(summary, step)
+        return self.log(summary, step)
 
     def flush(self):
         self.writer.flush()
+        return self
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='diautils',
-      version='1.3.3',
+      version='1.3.4',
       description='ML Utils',
       author='Eric Renault',
       author_email='eric.renaul.info@gmail.com',
