fcbfbd8d1f869362d3db665e8a4d7a6a60e6b9a1
Eric Renault <eric.renault.info@gmail.com>
2019-09-28 19:27:37 +0200

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='diautils',
-      version='1.3.6',
+      version='1.3.7',
       description='ML Utils',
       author='Eric Renault',
       author_email='eric.renaul.info@gmail.com',
